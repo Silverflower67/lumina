819cac1964558e38f788a1cd82d908e5c15585fa
Silverflower67 <sofimaturana830@gmail.com>
2022-08-31 15:29:48 -0400

    Some serializer updates

--- src/gmcp/ser/ser.rs
+++ src/gmcp/ser/ser.rs
@@ -1,6 +1,15 @@
-use serde::{ser,Serialize};
+use serde::Serializer;
 use super::error::{Error, Result};
 
-pub struct Serializer {
-    output: Vec<u8>,
+pub trait Data {
+    
+}
+
+pub fn to_gmcp<T>(value: &T) -> Result<Vec<u8>> where T: Serialize {
+    let data = serde_json::to_string(value)
+        .map_err(|err| Error::JSON(err))?;
+    let mut sent: Vec<u8> = vec![0xff,250,201];
+    sent.extend(data.as_bytes());
+    sent.extend([0xff,240]);
+    Ok(sent)
 }
